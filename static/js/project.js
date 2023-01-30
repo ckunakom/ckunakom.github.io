b85c5b2a8da73e671f905989641080a116f62d75
ckunakom <ckunakom@gmail.com>
2023-01-29 19:45:32 -0500

    Added a note on where will be a good place to use js; Started drafting object... religiously commiting

---
+++ static/js/project.js
@@ -0,0 +1,15 @@
+// --- Project Items --- //
+var content = [
+    {
+        title: "Data Mining: Smule App Data",
+        url: "https://ckunakom.github.io/my_smule_record/",
+        src: "img/data-mining.png",
+        alt: "data-mining",
+        description: "Mining raw data from Smule app in JSON format with API request and turn it to csv file using Python. Also loading data into an SQL server to look at them with simple SQL query."
+    },
+// list out the rest.
+// OY! don't forget to take into account that each line is 2 projects
+// So adjust ya loop accordingly with <div class="row pb-3 project-cards">
+// go steal code from edit check project or something.
+]
+
