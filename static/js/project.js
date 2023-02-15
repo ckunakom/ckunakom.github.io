75a5b3cca8b1cd9f4766667c8b54c295e30de2c1
ckunakom <ckunakom@gmail.com>
2023-02-14 19:25:39 -0500

    Started some work to convert pure html in project page to js; Updated project description

--- static/js/project.js
+++ static/js/project.js
@@ -5,7 +5,14 @@ var content = [
         url: "https://ckunakom.github.io/my_smule_record/",
         src: "img/data-mining.png",
         alt: "data-mining",
-        description: "Mining raw data from Smule app in JSON format with API request and turn it to csv file using Python. Also loading data into an SQL server to look at them with simple SQL query."
+        description: "My very first project after graduating from the bootcamp back in 2020. Mining raw data from Smule app in JSON format with API-like request and turn it to csv file using Python."
+    },
+    {
+        title: "Data Visualization: Smule App Data",
+        url: "https://ckunakom.github.io/smule-data-visual",
+        src: "img/data-visual.png",
+        alt: "data-visual",
+        description: "Analysis and visualization created from Tableau, using a more current and elaborated data extracted from the Smule app. A '2.0' version of the <a href='https://ckunakom.github.io/my_smule_record/' target='_blank'>Data Mining: Smule App Data</a> project."
     },
 // list out the rest.
 // OY! don't forget to take into account that each line is 2 projects
