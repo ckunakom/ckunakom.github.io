d5f161dbc296d0ff329638ea63f482f59c82a94e
ckunakom <ckunakom@gmail.com>
2023-02-18 13:15:51 -0500

    checking something iffy in css

--- static/js/project.js
+++ static/js/project.js
@@ -14,6 +14,20 @@ var content = [
         alt: "data-visual",
         description: "Analysis and visualization created from Tableau, using a more elaborated data extracted from the Smule app. A '2.0' version of the <a href='https://ckunakom.github.io/my_smule_record/' target='_blank'>Data Mining: Smule App Data</a> project."
     },
+    {
+        title: "My Top 50 Most Listened Tracks on Spotify",
+        url: "https://ckunakom.github.io/spotify-usage/",
+        src: "img/spotify.PNG",
+        alt: "spotify",
+        description: "Get an idea of what songs I have been listening to the most on Spotify these days. Probably a good indicator of what I will be singing!"
+    },
+    {
+        title: "Clinical Data Management: eCRF & Edit Checks",
+        url: "https://ckunakom.github.io/CRF_edit_checks/",
+        src: "img/ec_crf1.PNG",
+        alt: "crf_ec",
+        description: "Programmed edit checks on eCRF using javaScript to eliminate manual data review and data cleaning."
+    }
 // list out the rest.
 // OY! don't forget to take into account that each line is 2 projects
 // So adjust ya loop accordingly with <div class="row pb-3 project-cards">
